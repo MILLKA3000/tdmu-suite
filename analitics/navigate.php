a0840bb6f56499d6538a93ef9c1a7c29374f5934
semteacher <semteacher@gmail.com>
2013-04-11 16:06:24 +0300

    Add feature: retrieve teacher rating values from tdmu database

--- analitics/navigate.php
+++ analitics/navigate.php
@@ -1,18 +1,18 @@
 <?
 function navigate($title,$mas,$name)
 {
-echo "<br>".$title." <select class='".$name."' name='".$name."'>";
+echo "<br>".$title." <select style='width:800px;' class='".$name."' name='".$name."'>";
 echo "<option value='0' selected='selected'>".$title."</option>";
     for ($i=0;$i<count($mas);$i++)
     {
 	
 	if ($_GET[$name]==$mas[$i][0]||$_POST[$name]==$mas[$i][0])
 		{
-		echo "<option value='".$mas[$i][0]."' selected='selected'>".substr(strip_tags($mas[$i][1]),0,80)."</option>";
+		echo "<option value='".$mas[$i][0]."' selected='selected'>".substr(strip_tags($mas[$i][1]),0,120)."</option>";
 		}
 		else
 		{
-		echo "<option value='".$mas[$i][0]."'>".substr(strip_tags($mas[$i][1]),0,80)."</option>";
+		echo "<option value='".$mas[$i][0]."'>".substr(strip_tags($mas[$i][1]),0,120)."</option>";
 		}
  	}
 echo "</select>";
