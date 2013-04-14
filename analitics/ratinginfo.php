19eae1b81775db2d056e2c68085c51fd188ec46c
semteacher <semteacher@gmail.com>
2013-04-14 10:31:43 +0300

    improvement: added summary queries and conditions for a choice

--- analitics/ratinginfo.php
+++ analitics/ratinginfo.php
@@ -44,33 +44,53 @@ echo "<br><center><input type='submit' name='var' value='Вибрати'><br></form>";
 
     if (!$_POST['INDEXID']==0){
         if (!$_POST['DEPARTMENT']==0) {
-        $zag_mas =$base_tdmu->select("SELECT tk.kaf_name, tn.name, tiv.index_value
-                                    FROM `tr_teacher_indices_values` tiv
-                                    INNER JOIN `tbl_tech_name` tn ON tn.name_id = tiv.teacher_id
-                                    INNER JOIN `tbl_tech_journals` tj ON tj.name_id = tn.name_id
-                                    INNER JOIN `tbl_tech_kaf` tk ON tk.kaf_id = tj.kaf_id
-                                    WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) AND (tk.kaf_id=".$_POST['DEPARTMENT'].") order by tk.kaf_name, tn.name");	
-        } else {
-            $zag_mas =$base_tdmu->select("SELECT tk.kaf_name, tn.name, tiv.index_value
+            if (!$_POST['DETAIL']=="on") {
+                $detail_mas =$base_tdmu->select("SELECT tk.kaf_name, tn.name, tiv.index_value
                                         FROM `tr_teacher_indices_values` tiv
                                         INNER JOIN `tbl_tech_name` tn ON tn.name_id = tiv.teacher_id
                                         INNER JOIN `tbl_tech_journals` tj ON tj.name_id = tn.name_id
                                         INNER JOIN `tbl_tech_kaf` tk ON tk.kaf_id = tj.kaf_id
-                                        WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) order by tk.kaf_name, tn.name");    
+                                        WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) AND (tk.kaf_id=".$_POST['DEPARTMENT'].") order by tk.kaf_name, tn.name");
+            }
+            if (!$_POST['SUMMARY']=="on") {
+                $summary_mas =$base_tdmu->select("SELECT tk.kaf_name, SUM(tiv.index_value)
+                                        FROM `tr_teacher_indices_values` tiv
+                                        INNER JOIN `tbl_tech_name` tn ON tn.name_id = tiv.teacher_id
+                                        INNER JOIN `tbl_tech_journals` tj ON tj.name_id = tn.name_id
+                                        INNER JOIN `tbl_tech_kaf` tk ON tk.kaf_id = tj.kaf_id
+                                        WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) AND (tk.kaf_id=".$_POST['DEPARTMENT'].") order by tk.kaf_name");
+            }
+        } else {
+            if (!$_POST['DETAIL']=="on") {
+                $detail_mas =$base_tdmu->select("SELECT tk.kaf_name, tn.name, tiv.index_value
+                                                FROM `tr_teacher_indices_values` tiv
+                                                INNER JOIN `tbl_tech_name` tn ON tn.name_id = tiv.teacher_id
+                                                INNER JOIN `tbl_tech_journals` tj ON tj.name_id = tn.name_id
+                                                INNER JOIN `tbl_tech_kaf` tk ON tk.kaf_id = tj.kaf_id
+                                                WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) order by tk.kaf_name, tn.name");
+            }
+            if (!$_POST['SUMMARY']=="on") {
+                $detail_mas =$base_tdmu->select("SELECT tk.kaf_name, SUM(tiv.index_value)
+                                                FROM `tr_teacher_indices_values` tiv
+                                                INNER JOIN `tbl_tech_name` tn ON tn.name_id = tiv.teacher_id
+                                                INNER JOIN `tbl_tech_journals` tj ON tj.name_id = tn.name_id
+                                                INNER JOIN `tbl_tech_kaf` tk ON tk.kaf_id = tj.kaf_id
+                                                WHERE (tiv.index_id =".$_POST['INDEXID'].") AND (tiv.index_value >0) order by tk.kaf_name, tn.name");
+            }
         }
         echo" <table bgcolor='white' border=1 width = 100% class='ser'><tr><td colspan=3><center><b>".$ratingindex[$_POST['INDEXID']][1]."</b></td></tr>
     <tr><td><center><b>Назва кафедри</td><td><center><b>П.І.Б. викладача</td><td><center><b>Значення параметру</td></tr>";
-        for ($i=0;$i<count($zag_mas);$i++)
+        for ($i=0;$i<count($detail_mas);$i++)
         {
         echo "<tr>";
-            for($j=0;$j<count($zag_mas[0]);$j++)
+            for($j=0;$j<count($detail_mas[0]);$j++)
             {
                 if ($j<2) {
                     $cell_alignment = "<left>";
                 } else {
                     $cell_alignment = "<center>";
                 }
-                echo "<td>".$cell_alignment.$zag_mas[$i][$j]."</td>";
+                echo "<td>".$cell_alignment.$detail_mas[$i][$j]."</td>";
             }
         echo "</tr>";
         }
